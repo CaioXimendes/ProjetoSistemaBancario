c5dde31da2df4c42b60a3e0ecea04836207eb3c4
CaioFSX <CaioFSX@192.168.15.7>
2025-02-05 18:33:30 -0300

    Implementing JDBC Connection

--- src/main/java/ConexaoBancoDeDados/BancoDeDados.java
+++ src/main/java/ConexaoBancoDeDados/BancoDeDados.java
@@ -4,10 +4,20 @@
  */
 package ConexaoBancoDeDados;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author CaioFSX
  */
 public class BancoDeDados {
+    public static void main(String[]args) throws ClassNotFoundException, SQLException{
+    Class.forName("com.mysql.cj.jdbc.Driver");
     
+    try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/myDb", "user1", "pass")) {
+    // use con here
+    }
+    }
 }
