ea942303bdd1ab5024c8161ad1dadf6489378288
CaioFSX <CaioFSX@192.168.15.7>
2025-02-06 11:05:37 -0300

    finishing first implemention of Database through the code

--- src/main/java/ConexaoBancoDeDados/BancoDeDados.java
+++ src/main/java/ConexaoBancoDeDados/BancoDeDados.java
@@ -20,28 +20,26 @@ public class BancoDeDados {
     public void checarSeUsuarioContemCPF() throws ClassNotFoundException, SQLException {
         Usuario usu1 = new Usuario();
         Connection conexao1 = null;
-        try{
+        try {
             // use con here
             //USEM ISSO AQUI PARA DML EM BANCO DE DADOS
             ResultSet resultSet;
             Class.forName("com.mysql.cj.jdbc.Driver");
-            //Aqui é só alterar o link de conexão do MySQL
             conexao1 = DriverManager.getConnection("jdbc:mysql://192.168.15.8:3306/?user=caiofsx", "caiofsx", "database123");
             Statement statement = conexao1.createStatement();
-            resultSet = statement.executeQuery("SELECT cpfCliente from ContaBancaria where cpfCliente='"+usu1.getCpf()+"'");
-            if(resultSet.next()){
-                usu1.setCpf(resultSet.getString("cpfCliente"));
+            resultSet = statement.executeQuery("SELECT cpfCliente from ContaBancaria where cpfCliente='" + usu1.getCpf() + "'");
+            if (resultSet.next()) {
+                usu1.setCpf(resultSet.getString(""));
+                usu1.setUsuarioValido(true);
             }
-        }
-        catch (ClassNotFoundException ex){
+        } catch (ClassNotFoundException ex) {
             System.out.println("Driver do Banco de dados não localizado!");
-        }
-        catch (SQLException ex){
-            System.out.println("Erro durante a conexão com o banco de dados! Erro:"+ex.getMessage());
-        }
-        finally {
-            if(conexao1 != null){
+        } catch (SQLException ex) {
+            System.out.println("Erro durante a conexão com o banco de dados! Erro:" + ex.getMessage());
+        } finally {
+            if (conexao1 != null) {
                 conexao1.close();
             }
+        }
     }
 }
